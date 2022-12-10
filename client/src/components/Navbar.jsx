15f0124d44d7c9a6b3313162b7e6dd2d519d23f4
MuhammadFikriMaulana03 <fikrimaul0311@gmail.com>
2022-12-11 00:17:47 +0700

    Terakhir

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,97 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { Button } from './Button'
-import logo from "../assets/images/logo.png"
-import divider from "../assets/images/vector-1.png"
-import '../assets/scss/Navbar.scss'
-import { LogOut, initState } from "../features/authSlice"
-import { AiFillAppstore, AiFillHome, AiOutlineLogout, AiOutlineLogin } from "react-icons/ai";
-import { FaMapMarkedAlt } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { MdClose } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/logo.png';
+
+import divider from '../assets/images/vector-1.png';
+import '../assets/scss/Navbar.scss';
+import { LogOut, initState } from '../features/authSlice';
+import { AiFillAppstore, AiFillHome, AiOutlineLogout, AiOutlineLogin } from 'react-icons/ai';
+import { FaMapMarkedAlt } from 'react-icons/fa';
+
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Navbar = () => {
-  const [isMenuToggled, setIsMenuToggled] = useState(false)
-  const [button, setButton] = useState(true)
-  const { user } = useSelector((state) => state.auth)
-
-  const toggleMenu = () => {
-    setIsMenuToggled(!isMenuToggled)
-  }
-
+  const { user } = useSelector((state) => state.auth);
+  const [navState, setNavState] = useState(false);
   const closeMobileMenu = () => {
-    setIsMenuToggled(false)
-  }
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false)
-    } else {
-      setButton(true)
-    }
-  }
+    setIsMenuToggled(false);
+  };
 
   const logout = () => {
-    dispatch(LogOut())
-    dispatch(initState())
-    navigate("/")
-  }
-
-  useEffect(() => {
-    showButton()
-  }, [])
-
-  window.addEventListener('resize', showButton)
+    dispatch(LogOut());
+    dispatch(initState());
+    navigate('/');
+  };
 
   return (
-    <>
-      <nav className="navbar navbar-dark">
-        <Link to="/">
-          <img src={logo} width={100} height={50} alt="Logo Vital Maps" />
-        </Link>
-        <img className="divider" src={divider} alt="" />
-        <button className="navbar-toggler border-0" type="button" data-bs-toggle="modal" data-bs-target="#targetModal-item">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="navbar-container">
-          <div className="menu-icon" onClick={toggleMenu}>
-            <i className={isMenuToggled ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={isMenuToggled ? 'nav-menu active' : 'nav-menu'}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                <AiFillHome className="me-1 mb-1" /> Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/maps" className="nav-links" onClick={closeMobileMenu}>
-                <FaMapMarkedAlt className='mb-1 me-1'/> Maps
-              </Link>
-            </li>
-            {user ? (
+    <nav>
+      <div className="brand-container">
+        <div className="brand">
+          <Link to="/">
+            <img src={logo} width={100} height={50} padding={10} alt="Logo Vital Maps" />
+          </Link>
+        </div>
+        <div className="toggle-container">
+          <div className="toggle">{navState ? <MdClose onClick={() => setNavState(false)} /> : <GiHamburgerMenu onClick={() => setNavState(true)} />}</div>
+        </div>
+      </div>
+      <div className={`links-container ${navState ? 'nav-visible' : ''}`}>
+        <ul className="links">
+          <li>
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              <AiFillHome className="me-1 mb-1" /> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/maps" className="nav-links" onClick={closeMobileMenu}>
+              <FaMapMarkedAlt className="mb-1 me-1" /> Maps
+            </Link>
+          </li>
+          {user ? (
             <>
-            <li className="nav-item">
-              <Link to="/dashboard" className="nav-links" onClick={closeMobileMenu}>
-                <AiFillAppstore className='mb-1 me-1' /> Dashboard
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={logout}>
-                <AiOutlineLogout className='me-1 mb-1' /> Logout
-              </Link>
-            </li>
-            </>
-            ) : (
               <li className="nav-item">
-                <Link to="/login" className="nav-links" onClick={closeMobileMenu}>
-                <AiOutlineLogin className="me-1 mb-1" /> Login
+                <Link to="/dashboard" className="nav-links" onClick={closeMobileMenu}>
+                  <AiFillAppstore className="mb-1 me-1" /> Dashboard
                 </Link>
               </li>
-            )}
-          </ul>
-        </div>
-      </nav>
-    </>
-  )
-}
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={logout}>
+                  <AiOutlineLogout className="me-1 mb-1" /> Logout
+                </Link>
+              </li>
+            </>
+          ) : (
+            <li className="nav-item">
+              <Link to="/login" className="nav-links" onClick={closeMobileMenu}>
+                <AiOutlineLogin className="me-1 mb-1" /> Login
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/pages/admin/Hospital.jsx
+++ client/src/pages/admin/Hospital.jsx
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getHospitals, deleteHospital } from '../../features/mapSlice'
-import Sidebar from "../../components/Sidebar"
-import { Link } from "react-router-dom"
-import "../../assets/scss/Hospital.scss"
-import { FaTrash, FaPen } from "react-icons/fa";
-import { AiOutlinePlus } from "react-icons/ai";
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getHospitals, deleteHospital } from '../../features/mapSlice';
+import Sidebar from '../../components/Sidebar';
+import { Link } from 'react-router-dom';
+import '../../assets/scss/Hospital.scss';
+import { FaTrash, FaPen } from 'react-icons/fa';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const Hospital = () => {
-  const dispatch = useDispatch()
-  const { hospitals } = useSelector((state) => state.map)
+  const dispatch = useDispatch();
+  const { hospitals } = useSelector((state) => state.map);
 
   useEffect(() => {
-    dispatch(getHospitals())
-  }, [dispatch])
+    dispatch(getHospitals());
+  }, [dispatch]);
 
-  function deleteData(slug){
-    dispatch(deleteHospital(slug))
-    dispatch(getHospitals())
+  function deleteData(slug) {
+    dispatch(deleteHospital(slug));
+    dispatch(getHospitals());
   }
 
   return (
-    <section className="h-100 w-100" style={{boxSizing: 'border-box', backgroundColor: '#FFFFFF'}}>
+    <section className="hospital h-100 w-100" style={{ boxSizing: 'border-box', backgroundColor: '#FFFFFF' }}>
       <div className="container-fluid mx-auto p-0 position-relative">
         <Sidebar />
         <div className="content">
           <div className="recently d-flex flex-column">
             <h2 className="content-title">Recent Hospital Data</h2>
             <Link to="/hospital/create">
-              <button className='btn btn-primary'><AiOutlinePlus className='mb-1'/> Tambah Data</button>
+              <button className="btn btn-primary">
+                <AiOutlinePlus className="mb-1" /> Tambah Data
+              </button>
             </Link>
             {/* Table Start */}
             <table className="table table-responsive table-striped">
@@ -42,27 +44,26 @@ const Hospital = () => {
                 </tr>
               </thead>
               <tbody>
-                {hospitals ?
-                  hospitals.length != 0 ? 
-                    hospitals.map((hospital, i) => (
-                      <tr key={i}>
-                        <td className='text-center'>{++i}</td>
-                        <td>{hospital.title}</td>
-                        <td>{hospital.alamat}</td>
-                        <td className='text-center'>{hospital.phone}</td>
-                        <td>
-                          <div className='d-flex justify-content-center align-items-center'>
-                            <Link to={'/hospital/'+hospital.slug+'/edit'}>
-                            <FaPen className='edit' />
-                            </Link>
-                            <FaTrash className='delete' onClick={() => deleteData(hospital.slug)} />
-                          </div>
-                        </td>
-                      </tr>
-                    ))
+                {hospitals
+                  ? hospitals.length != 0
+                    ? hospitals.map((hospital, i) => (
+                        <tr key={i}>
+                          <td className="text-center">{++i}</td>
+                          <td className="text-center">{hospital.title}</td>
+                          <td className="text-center">{hospital.alamat}</td>
+                          <td className="text-center">{hospital.phone}</td>
+                          <td>
+                            <div className="d-flex justify-content-center align-items-center">
+                              <Link to={'/hospital/' + hospital.slug + '/edit'}>
+                                <FaPen className="edit" />
+                              </Link>
+                              <FaTrash className="delete" onClick={() => deleteData(hospital.slug)} />
+                            </div>
+                          </td>
+                        </tr>
+                      ))
                     : null
-                  : null
-                }
+                  : null}
               </tbody>
             </table>
             {/* Table End */}
@@ -70,7 +71,7 @@ const Hospital = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hospital
+export default Hospital;
